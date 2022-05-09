13bdd11abe594efbe039b4ba0514a3c838716ed0
Subodha Sahu <subodha.sahu@rapidfunnel.com>
2022-05-09 13:35:26 +0530

    Add and Remove Todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Another from './Another';
 import '../App.css';
+import userEvent from '@testing-library/user-event';
 
 function App() {
+  const regInput = React.useRef();
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -20,26 +22,54 @@ function App() {
       isComplete: false,
     },
   ]);
+  const [todoId, setTodoId] = useState(todos.length);
+  const [todoInput, setTodoInput] = useState('');
+  let addTodo = (event) => {
+    event.preventDefault()
+    if (todoInput.trim().length === 0) {
+      return;
+    }
+
+
+    const newTodo = {
+      id: todoId + 1,
+      title: todoInput,
+      isComplete : false
+    }
+    setTodoId(todoId => todoId + 1);
+    setTodos(todos => [...todos, newTodo]);
+    setTodoInput('');
+  }
+  let updateTodoInput = (element) => {
+    setTodoInput(element.target.value)
+  }
+
+  let deleteTodo = (todoId) => {
+    setTodos(todos => [...todos].filter(todo => todo.id !== todoId))
+  }
   return (
     <div className="todo-app-container">
         <div className="todo-app">
         <h2>Todo App</h2>
-        <form action="#">
+        <form action="#" onSubmit={addTodo}>
             <input
-              type="text"
+            type="text"
+           // ref={regInput}
+            value={todoInput}
+            onChange={updateTodoInput}
               className="todo-input"
               placeholder="What do you need to do?"
             />
           </form>
       <ul>
         {todos.map((todo, index) => (
-        <li className="todo-item-container">
+        <li className="todo-item-container" key={todo.id}>
         <div className="todo-item">
           <input type="checkbox" />
           <span className="todo-item-label">{todo.title}</span>
           {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
         </div>
-        <button className="x-button">
+        <button className="x-button" onClick={() => deleteTodo(todo.id)}>
           <svg
             className="x-button-icon"
             fill="none"
